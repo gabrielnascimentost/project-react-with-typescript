8198f28815123a1e9baaa60f1242da9d6c5f791b
Gabriel Nascimento <gabrielstnascimento@outlook.com>
2021-04-22 17:14:27 -0300

    amend on layout

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/img/logo.svg';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-    return <Title>Explore repositórios no Github</Title>;
+    return (
+        <>
+            <img src={logoImg} alt="GitHub Explorer" />
+            <Title>Explore repositórios no Github</Title>
+
+            <Form>
+                <input placeholder="Digite o nome do repositório" />
+                <button type="submit">Pesquisar</button>
+            </Form>
+
+            <Repositories>
+                <a href="teste">
+                    <img
+                        src="https://avatars.githubusercontent.com/u/48933391?v=4"
+                        alt="Gabriel Nascimento"
+                    />
+                    <div>
+                        <strong>project-react/typescript</strong>
+                        <p>
+                            Easy project to start programming with typescript on
+                            react
+                        </p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </a>
+            </Repositories>
+        </>
+    );
 };
 
 export default Dashboard;
