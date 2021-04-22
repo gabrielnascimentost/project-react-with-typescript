f04c298375917763dab88c6d5b710dea5ce67649
Gabriel Nascimento <gabrielstnascimento@outlook.com>
2021-04-22 11:17:05 -0300

    config routes in the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-function App() {
-  return (
-    <h1>Hello World</h1>
-  );
-}
+import Routes from './routes';
+
+const App: React.FC = () => <Routes />;
 
 export default App;
